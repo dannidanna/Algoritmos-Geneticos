ca26628a539ac4a62b0bedf45e807d8246d7577f
Danny Rodriguez <dannidanna01@gmail.com>
2016-08-05 12:02:30 -0400

    Proyecto en netbeans

---
+++ Optimizacion de tela - Alg. Geneticos/src/optimizacion/de/tela/alg/geneticos/OptimizacionDeTelaAlgGeneticos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package optimizacion.de.tela.alg.geneticos;
+
+/**
+ *
+ * @author Danny
+ */
+public class OptimizacionDeTelaAlgGeneticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
