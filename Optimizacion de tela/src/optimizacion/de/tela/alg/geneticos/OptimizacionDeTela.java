839ef340614e39364c5576e289c6e71862be5173
Danny Rodriguez <dannidanna01@gmail.com>
2016-08-05 20:45:09 -0400

    Interface

--- Optimizacion de tela/src/optimizacion/de/tela/alg/geneticos/OptimizacionDeTela.java
+++ Optimizacion de tela/src/optimizacion/de/tela/alg/geneticos/OptimizacionDeTela.java
@@ -9,13 +9,13 @@ package optimizacion.de.tela.alg.geneticos;
  *
  * @author Danny
  */
-public class OptimizacionDeTelaAlgGeneticos {
+public class OptimizacionDeTela {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Interface admin = new Interface();
     }
     
 }
